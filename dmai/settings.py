2a320d08e7796ed164eb51fc9eaec3dfc6cfe157
nsomazr <zreuben815@gmail.com>
2023-02-02 22:39:25 +0300

    final-demo

--- dmai/settings.py
+++ dmai/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q2v7+5!r_6p_4zu2915d4zudy38p7z3@#pjo#c30@^4rhwe*!-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
