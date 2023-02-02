0960a4bb20f5aef50e72b5fd31690f25deba069c
nsomazr <zreuben815@gmail.com>
2023-02-02 17:36:23 +0300

    static files

--- backend/urls.py
+++ backend/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import InferenceAPIView, DataInferenceAPIView
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 app_name = "backend"  
 
 urlpatterns = [
                 path('inference/api/', DataInferenceAPIView.as_view(), name="inference-api"),
                 path('results/api', InferenceAPIView.as_view(), name="results-api"),
                 path('predict/', views.prediction, name="predict")
-                 ]
+                 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- frontend/urls.py
+++ frontend/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import FrontendAPIView
-
+from django.conf import settings
+from django.conf.urls.static import static
 app_name = "frontend"  
 
 urlpatterns = [
                 path('', FrontendAPIView.index, name = 'index'),
                 # path('home/api/', FrontendAPIView.as_view(), name="home-api")
-                ]
+                ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
